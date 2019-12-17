0e4bb7c77f1ec44b096573cfa89e06dc300e1af4
srikanth556 <srikanth.mamindla@gmail.com>
2019-12-17 16:04:51 +0800

    Update ValuesController.cs
    changes to test CI

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,14 +14,14 @@ namespace JenkiTest.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "srikanth", "Goud" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "value";
+            return "srikanth its your value "+id;
         }
 
         // POST api/values
